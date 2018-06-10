e7af92f1398501e0eb08ff2ce9fe351d1d413002
Joana Coutinho <joanacoutinho01@gmail.com>
2018-06-10 22:28:22 +0100

    Ex 1 com comentarios

--- Ex1.py
+++ Ex1.py
@@ -1,8 +1,13 @@
 dict = {3: "Fizz", 5: "Buzz"}
-
-def printNumbers(dict):
+n = 100
+def printNumbers(dict,n):
+    """
+    Imprime a string "value" do dicionario se a sua "key" for multipla de
+    um numero de 1 a n. Imprime o proprio numero caso contrario. Se houver
+    mais de 1 multiplo, este e tambem impresso.
+    """
     s = ""
-    for i in range(1,101):
+    for i in range(1, n+1):
         for j in dict.keys():
             if i%j == 0:
                 s += dict[j] + " "
@@ -12,4 +17,4 @@ def printNumbers(dict):
             print(i)
         s = ""
 
-printNumbers(dict)
+printNumbers(dict,n)
